9da6ba0570c4f9c213643061d142e37e8a96acb7
USAC-AnthonyZea <3661462700115@ingenieria.usac.edu.gt>
2022-09-30 01:45:54 -0600

    Lectura de primer archivo XML

--- main.py
+++ main.py
@@ -1,44 +1,63 @@
-from colorama import Fore, Style
+# Creamos clase Nodo, para todas las listas
+class Nodo():
+    def __init__(self, dato = None, siguiente = None):
+        self.dato = dato
+        self.siguiente = siguiente
 
-class App():
-    def __init__(self):
+# Creamos clase para empresas
+class Empresas():
 
-        # Creacion del Menu
-        print(Style.BRIGHT + Fore.WHITE + '\n-------- Soluciones Guatemaltecas, S.A -----------')
-        print(Style.BRIGHT + Fore.YELLOW + '_Bienvenido al sistema:_\n')
+    def __init__(self, id, nombre, abreviatura):
+        self.id = id
+        self.nombre = nombre
+        self.abreviatura = abreviatura
+        self.lista_punto_atencion = Lista_enlazada()
+        self.lista_transacciones = Lista_enlazada()
 
-        print(Style.BRIGHT + Fore.WHITE + "-------------------- MENU ------------------------\n")
-        print(Style.NORMAL + Fore.WHITE +'1. Configuración de Empresas')
-        print('2. Selección de empresa y punto de atención')
-        print('3. Manejo de puntos de atención')
-        print('4. Salir')
-        opcion = input(Style.NORMAL + Fore.GREEN +'\nIngrese la operacion que desea realizar: ')
+# Creamos clase para puntos de atencion
+class Puntos_atencion():
+    def __init__(self, id_punto, nombre_punto, direccion_punto):
+        self.id_punto = id_punto
+        self.nombre_punto = nombre_punto
+        self.direccion = direccion_punto
+        self.lista_escritorios = Lista_enlazada()
 
-        if opcion == '1':
-            print(Style.BRIGHT + Fore.WHITE +'\n------------- Configuración de Empresas ------------------')
-            print(Style.NORMAL + Fore.WHITE + "\n1. Limpiar Sistema")
-            print("2. Cargar archivo de configuración del sistema")
-            print("3. Crear Nueva empresa")
-            print("4. Cargar archivo con configuración Inicial para la prueba")
-            opcion_1 = input(Style.NORMAL + Fore.GREEN +'\nIngrese la operacion que desea realizar: ')
+# Creamos clara para trasnacciones
+class Transacciones():
+    def __init__(self, id_trans, nombre_trans, tiempo_trans):
+        self.id_trans = id_trans
+        self.nombre_trans = nombre_trans
+        self.tiempo_trans = tiempo_trans
 
-            if opcion_1 == '2':
-                print()
+class Escritorios():
+    def __init__(self, id_escritorio, identificacion, encargado):
+        self.id_escritorio = id_escritorio
+        self.identificacion = identificacion
+        self.encargado = encargado
         
-        elif opcion == '2':
-            print('\n----- Selección de empresa y punto de atención --------')
-            print("\na) Seleccione la empresa: ")
-            print("b) Seleccione el punto de atención: ")
+# ---------------- Aqui viene todo el TDA (Linked List) -------------------- #
+class Lista_enlazada():
 
-        elif opcion == '3':
-            print('\n---------------- Manejo de puntos de atención -----------------------')
-            print("\n1. Ver estado del punto de atencion")
-            print("2. Activar o Desactivar escritorio de servicio (1 ó 0, respectivamente)")
-            print("3. Atender Cliente")
-            print("4. Solicitud de Atención")
-        else:
-            print(Style.BRIGHT + Fore.LIGHTRED_EX + '\nNo ha ingresado una opcion correcta')
+    def __init__(self) -> None:
+        self.raiz = None # Definimos la cabeza
+    
+    # Metodo para agregar datos al inicio
+    def append_inicio(self, dato_):
+        self.raiz = Nodo(dato = dato_, siguiente = self.raiz)
 
+    # Metodo para agregar datos al final
+    def append_final(self, dato_):
+        if not self.raiz:
+            self.raiz = Nodo(dato = dato_)
+            return
+        actual = self.raiz
+        while actual.siguiente:
+            actual = actual.siguiente
+        actual.siguiente = Nodo(dato = dato_)
 
-if __name__ == "__main__":
-    App()
+    # Método para imprimir la lista de nodos
+    def imprimir_lista(self, llave):
+        nodo = self.raiz
+        while nodo != None:
+            print(getattr(nodo.dato, llave), " => ")
+            nodo = nodo.siguiente
--- xmlFile.py
+++ xmlFile.py
@@ -1,4 +1,5 @@
 import xml.etree.cElementTree as ET
+from main import *
 
 class XML_entry():
 
@@ -6,7 +7,48 @@ class XML_entry():
         root = 'configXml.xml' #Ruta del archivo
 
         tree = ET.parse(root) #Parseas el archivo Xml
-        listaEmpresas = tree.getroot() # Obtener la lista de empresas
-    
-    def test_System():
-        print()
+        empresas = tree.getroot() # Obtener la lista de empresas
+        listaEmpresasXml = Lista_enlazada()
+
+        # Iteramos sobre las empresas
+        for empresa in empresas:
+            id_empresa = empresa.get('id') # Obtenemos el id de empresa
+            nombre_empresa = empresa.find('nombre').text # Obtenemos el nombre de empresa
+            abreviatura = empresa.find('abreviatura').text # Obtenemos la abreviatura de empresa
+
+            nueva_Empresa = Empresas(id_empresa, nombre_empresa, abreviatura)
+
+            listaEmpresasXml.append_inicio(nueva_Empresa)
+
+            # Iteramos sobre la lista de puntos de atencion
+            for puntos_Atencion in empresa.iter('listaPuntosAtencion'):
+
+                # Iteramos sobre el punto de atencion especifico
+                for punto_atencion in puntos_Atencion.iter('puntoAtencion'):
+                    id_punto = punto_atencion.get('id') # Obtenemos el id de punto de atencion
+                    nombre_punto = punto_atencion.find('nombre').text # Obtenemos el nombre 
+                    direccion = punto_atencion.find('direccion').text # Obtenemos la direccion
+
+                    nuevo_punto_atencion = Puntos_atencion(id_punto, nombre_punto, direccion)
+                    nueva_Empresa.lista_punto_atencion.append_inicio(nuevo_punto_atencion)
+
+                    # Iteramos sobre la lista de escritorios
+                    for lista_Escritorios in punto_atencion.iter('listaEscritorios'):
+
+                        # Iteramos sobre el escritorio especifico
+                        for escritorio in lista_Escritorios.iter('escritorio'):
+                            id_escritorio = escritorio.get('id')
+                            identify_escritorio = escritorio.find('identificacion').text
+                            encargado_escritorio = escritorio.find('encargado').text
+
+                            nuevo_Escritorio = Escritorios(id_escritorio, identify_escritorio, encargado_escritorio)
+                            nuevo_punto_atencion.lista_escritorios.append_inicio(nuevo_Escritorio)
+        
+        print(listaEmpresasXml.imprimir_lista('nombre'))
+
+
+XML_entry.config_System()
+
+
+    #def test_System():
+    #    print()
